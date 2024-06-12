1cae81fc26799f7b3815e09a99d794e6ec4e97bc
vladislav-vavilov <maksimsraud@gmail.com>
2024-06-12 20:01:38 +0300

    feat: added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { Settings } from './pages/Settings'
+import { Post } from './pages/Post'
+
 const App = () => {
-  return <span className='text-red-500'>Hello world</span>
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/posts'>
+        <Route path=':postId' element={<Post />} />
+      </Route>
+      <Route path='/settings' element={<Settings />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/pages/Post.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react'
+
+export const Post: FC = () => {
+  return <div>Post</div>
+}
